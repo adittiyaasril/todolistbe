fda8326ccf4181e6e79a7284a7e639c46481e570
adittiyaasril <adittiyaasril369@gmail.com>
2023-08-10 16:42:04 +0700

    vercel

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+
+app.enable("trust proxy");
 app.use(
   cors({
     origin: "https://todolistfe-seven.vercel.app", // Replace with your frontend URL
