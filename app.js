6da25203cc3c77beb69de9d1f57e7c8075946928
adittiyaasril <adittiyaasril369@gmail.com>
2023-08-10 18:26:54 +0700

    vercel

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: "https://todolistfe-seven.vercel.app", // Replace with your frontend URL
     credentials: true,
   })
 );
